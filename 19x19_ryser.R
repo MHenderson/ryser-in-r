2f27dfdd7d3850ba0c192fb3cab71b95a13863e7
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-03-01 10:29:58 +0000

    highlight original rectangle

--- 19x19_ryser.R
+++ 19x19_ryser.R
@@ -4,12 +4,18 @@ library(tidyverse)
 
 list.files("R", full.names = TRUE) %>% walk(source)
 
-expand_grid(row = 1:3, column = 1:4) %>%
-  mutate(symbol = c(1, 3, 4, 2, 5, 6, 1, 3, 4, 2, 3, 5)) %>%
+R <- expand_grid(row = 1:3, column = 1:4) %>%
+  mutate(
+    symbol = c(1, 3, 4, 2, 5, 6, 1, 3, 4, 2, 3, 5),
+    original = TRUE
+  ) %>%
   add_cols(5:19, 19) %>%
-  add_rows(4:19) %>%
+  add_rows(4:19)
+
+R %>%
   ggplot(aes(column, row)) +
   geom_tile(aes(fill = symbol)) +
+  geom_text(data = R %>% filter(original), aes(label = symbol), size = 2, colour = "white") +
   scale_y_reverse() +
   coord_fixed() +
   theme_void() +
