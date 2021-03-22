2d9a67ed07ce1077d6ed5bd4c7fff9668788e733
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-03-22 15:52:32 +0000

    variation on the 19x19 example

--- 19x19_ryser.R
+++ 19x19_ryser.R
@@ -4,18 +4,22 @@ library(tidyverse)
 
 list.files("R", full.names = TRUE) %>% walk(source)
 
-R <- expand_grid(row = 1:3, column = 1:4) %>%
+expand_grid(row = 1:3, column = 1:4) %>%
   mutate(
-    symbol = c(1, 3, 4, 2, 5, 6, 1, 3, 4, 2, 3, 5),
-    original = TRUE
+    symbol = c(1, 3, 4, 2, 5, 6, 1, 3, 4, 2, 3, 5)
   ) %>%
   add_cols(5:19, 19) %>%
-  add_rows(4:19)
-
-R %>%
+  add_rows(4:19) %>%
+  mutate(
+    original = case_when(
+      row <= 3 & column <= 4 ~ TRUE,
+      TRUE ~ FALSE
+    )
+  ) %>%
   ggplot(aes(column, row)) +
   geom_tile(aes(fill = symbol)) +
-  geom_text(data = R %>% filter(original), aes(label = symbol), size = 2, colour = "white") +
+  geom_text(data = . %>% filter(original), aes(label = symbol), size = 3, colour = "red") +
+  geom_text(data = . %>% filter(!original), aes(label = symbol), size = 2, colour = "white") +
   scale_y_reverse() +
   coord_fixed() +
   theme_void() +
